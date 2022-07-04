9eed5ad072f4389485a7c995288ee2605fd94d4f
Sergey <santexnik.86@mail.ru>
2022-07-04 15:53:33 +0300

    Задача 19

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(" Введите пятизначное число: ");
+string number = Console.ReadLine();
+Zadacha19(number);
+void Zadacha19(string curnumber)
+{
+    if (curnumber[0] == curnumber[4] && curnumber[1] == curnumber[3])
+
+    Console.WriteLine(" Число является полиндромом ");
+    else Console.WriteLine(" Простое пятизначное число");
+}
