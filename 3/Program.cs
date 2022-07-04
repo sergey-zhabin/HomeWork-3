4bcc39b22ccfd981ecb5becf6ae80b244d707ad5
Sergey <santexnik.86@mail.ru>
2022-07-04 15:33:16 +0300

    Задача 23

---
+++ 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Zadacha23(N);
+
+void Zadacha23(int cN)
+{
+    int count = 1;
+    while (count <= cN)
+    {
+        Console.Write(count*count*count +" ");
+        count++;
+    }
+}
